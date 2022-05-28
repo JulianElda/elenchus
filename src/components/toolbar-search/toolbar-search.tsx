a316b3b09d085ed895b45d7ee793d51ee485d4c7
Julius Polar <julius.polar@protonmail.com>
2022-05-28 15:00:56 +0200

    added filter api

--- src/components/toolbar-search/toolbar-search.tsx
+++ src/components/toolbar-search/toolbar-search.tsx
@@ -1,13 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+
+import axios from "api/axios";
+import { Entry, IdgardBox } from "types";
+
+import FileList from "components/file-list/file-list";
 
 export default function ToolbarSearch(props) {
   const { handleSubmit } = useForm();
   const [query, setQuery] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<Array<Entry>>([]);
+  const [boxList, setBoxList] = useState<Array<IdgardBox>>([]);
 
   const onSearch = () => {
-    console.log(query);
+    //findItemsInBox()
   };
+
+  const findItemsInBox = (boxId) => {
+    // TODO: types to radio select
+    let types = "types=file,folder,note";
+    let name = "name=" + query;
+
+    axios
+      .get(
+        "/uiapi/FilterAPI/v1/rest/boxes/" +
+          boxId +
+          "/nodes?" +
+          types +
+          "&" +
+          name
+      )
+      .then((res) => {
+        setSearchResult(
+          res.data.map(function (item) {
+            return {
+              id: item.node.id,
+              type: item.type,
+              name: item.node.name,
+            };
+          })
+        );
+      })
+      .catch((res) => {});
+  };
+
+  useEffect(() => {
+    axios
+      .get("/uiapi/BoxAPI/v1/rest/boxes")
+      .then((res) => {
+        setBoxList(res.data);
+      })
+      .catch((res) => {});
+  }, []);
 
   return (
     <>
@@ -36,6 +80,8 @@ export default function ToolbarSearch(props) {
                 </div>
               </form>
             </div>
+
+            <FileList items={searchResult} />
           </div>
         </div>
       </div>
